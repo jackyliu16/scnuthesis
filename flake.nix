ad6163e5432734027b6957135b1a8be66e84405f
jackyliu16 <18922251299@163.com>
2024-04-05 01:30:28 +0800

    impl flake support

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    latex-utils = {
+      # url = "github:jackyliu16/latex-utils";
+      url = "git+file:///home/jacky/Documents/latex-utils/";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    latex-utils,
+  }:
+    with flake-utils.lib; eachSystem allSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+      texPackages = {
+        # NOTE: add some latex package you want 
+        inherit (pkgs.texlive) xecjk xetex bibtex;
+      };
+    in {
+      packages.default = latex-utils.lib.${system}.mkLatexPdfDocument {
+        name = "mydocument";
+        src = self;
+        inherit texPackages;
+        inputFile = "main.tex";
+
+        fonts = [
+          pkgs.times-newer-roman # not working
+          "${./fonts}"
+        ];
+
+        buildPhase = ''
+          xelatex main.tex 
+          bibtex main.aux
+          xelatex main.tex # not sure if correct
+          xelatex main.tex 
+        '';
+        installPhase = ''
+          mv main.pdf $out 
+        '';
+      };
+    });
+}
